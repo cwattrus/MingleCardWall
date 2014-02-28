f4df169d14d720d1cc23d980c15b1b29efc32ce7
tworker <tworker@thoughtworks.com>
2014-02-27 17:53:52 -0800

    Saving feedback and adjusting height of beta box

--- cardwall/client/feedback/feedback.js
+++ cardwall/client/feedback/feedback.js
@@ -2,11 +2,30 @@ if (Meteor.isClient) {
   Template.feedback.feedbacktime = function () {
   	return Session.get("feedback_time");
   };
+  Template.feedback.feeling = function() {
+  	return Session.get("feeling");
+  }
 
   Template.feedback.events({
     'click .exit-feedback' : function() {
       Session.set("feedback_time", false);
       Logs.insert({"action": "Close feedback", "user": Meteor.user()._id});
     },
+    'click #save_feedback' : function() {
+    	Logs.insert({"action": "Feedback", "user" : Meteor.user()._id, "text" : $("#feedback_text").val(), "feeling" : Session.get("feeling") });
+      	Session.set("feedback_time", false);
+    },
+    'click #meh' : function() {
+		Session.set("feeling", "meh");
+   	},
+   	'click #never' : function() {
+		Session.set("feeling", "never");
+   	},
+   	'click #amped' : function() {
+    	Session.set("feeling", "amped");
+   	},
+   	'click #dreaming' : function() {
+    	Session.set("feeling", "dreaming");
+   	},
   });
 }
